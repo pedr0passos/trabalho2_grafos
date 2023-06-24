7650addf217db806ad36c4e23972a44042ae7d7e
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2023-06-24 19:58:19 -0300

    Melhora da Eficiencia

--- trab/main.c
+++ trab/main.c
@@ -11,9 +11,10 @@ typedef struct a {
 void r_graph(FILE* input,int n, int m, int matriz[n][n], aresta *a);
 void inicializa(int n, int matriz[n][n], int matriz_parcial[n][n]);
 int arvore_minima(int n, int m, int matriz[n][n], int matriz_parcial[n][n], aresta *a);
-void b_insertionsort(aresta *v, int m);
-void atualiza_florestas(int n, int *florestas, int matriz[n][n]);
-void conta(int n, int matriz[n][n], int vertice, int *florestas, int cont);
+void quicksort(aresta *v, int esquerda, int direita);
+void particao(aresta *v, int esquerda, int direita, int *i, int*j);
+int atualiza_arvores(int n, int *arvores, int matriz[n][n]);
+void conta(int n, int matriz[n][n], int vertice, int *arvores, int cont);
 void info(int resultado, int n, int matriz[n][n]);
 
 int main () {
@@ -22,7 +23,7 @@ int main () {
     int n, m;
 
     // lendo valores n e m
-    FILE *input=fopen("input/9.txt", "r");
+    FILE *input=fopen("input/3.txt", "r");
     fscanf(input, "%d %d\n", &n, &m);
 
     // criando matriz e vetor com as arestas
@@ -31,9 +32,9 @@ int main () {
 
     inicializa(n,matriz, matriz_parcial);
     r_graph(input,n,m,matriz, arestas);
-    b_insertionsort(arestas, m);
+    quicksort(arestas, 0, m-1);
     resultado = arvore_minima(n,m,matriz,matriz_parcial,arestas);
-    info(resultado, n,matriz_parcial);
+    info(resultado, n, matriz_parcial);
     
     return 0;
 }
@@ -47,23 +48,32 @@ void inicializa(int n, int matriz[n][n], int matriz_parcial[n][n]) {
         }
 }
 
-void b_insertionsort(aresta *v, int m) {
-    int  i, j, esquerda, direita, meio;
-    aresta auxiliar;
-    for (i = 1; i < m; i++) {
-        auxiliar = v[i];
-        esquerda = 0;
-        direita = i;
-        while ( esquerda < direita ) {
-            meio = ( esquerda + direita )/2;
-            if ( v[meio].peso <= auxiliar.peso )    
-                esquerda = meio + 1;
-            else    
-                direita = meio;
+void quicksort(aresta *v, int esquerda, int direita) {
+    int i, j;
+    particao(v,esquerda,direita, &i, &j);
+    if ( i < direita )
+        quicksort(v, i, direita);
+    if ( j > esquerda ) 
+        quicksort(v, esquerda, j);
+}
+
+void particao(aresta *v, int esquerda, int direita, int *i, int*j) {
+    aresta aux, pivo;
+    pivo = v[direita];
+    *i = esquerda;
+    *j = direita;
+    while ( (*i) <= (*j) ) {
+        while (v[*i].peso < pivo.peso && (*i) < direita)
+            (*i)++;
+        while ( v[*j].peso > pivo.peso && (*j) > esquerda )
+            (*j)--;
+        if ( (*i) <= (*j) ) {
+            aux = v[*i];
+            v[*i] = v[*j];
+            v[*j] = aux;
+            (*i)++;
+            (*j)--;
         }
-        for (j=i; j > esquerda; j--)
-            v[j] = v[j-1];
-        v[direita] = auxiliar;        
     }
 }
 
@@ -92,32 +102,33 @@ void r_graph(FILE* input, int n, int m, int matriz[n][n], aresta *a) {
 }
 
 int arvore_minima(int n, int m, int matriz[n][n], int matriz_parcial[n][n], aresta *a) {
-    int i, florestas[n], soma=0;
-    atualiza_florestas(n,florestas,matriz_parcial);
-    for (i=0; i<m; i++)
-        if ( florestas[a[i].u] != florestas[a[i].v] ) {
+    int i, arvores[n], soma=0, qt_arvores;
+    qt_arvores=atualiza_arvores(n,arvores,matriz_parcial);
+    for (i=0; i<m || qt_arvores>1; i++)
+        if ( arvores[a[i].u] != arvores[a[i].v] ) {
             matriz_parcial[a[i].u][a[i].v]=a[i].peso;
             matriz_parcial[a[i].v][a[i].u]=a[i].peso;
             soma += a[i].peso;
-            atualiza_florestas(n,florestas,matriz_parcial);
+            qt_arvores=atualiza_arvores(n,arvores,matriz_parcial);
         }
     return soma;
 }
 
-void atualiza_florestas(int n, int *florestas, int matriz[n][n]) {
+int atualiza_arvores(int n, int *arvores, int matriz[n][n]) {
     int cont=0, i;
     for (i=0; i<n; i++)
-        florestas[i]=-1;
+        arvores[i]=-1;
     for (i=0; i<n; i++)
-        if(florestas[i]==-1) {
+        if(arvores[i]==-1) {
             cont++;
-            conta(n, matriz, i, florestas, cont);
+            conta(n, matriz, i, arvores, cont);
         }
+    return cont;
 }
 
-void conta(int n, int matriz[n][n], int vertice, int *florestas, int cont) {
-    florestas[vertice] = cont;
+void conta(int n, int matriz[n][n], int vertice, int *arvores, int cont) {
+    arvores[vertice] = cont;
     for(int j=0; j<n; j++)
-        if ( matriz[vertice][j] > 0 && florestas[j] == -1)
-                conta(n, matriz, j, florestas, cont);
+        if ( matriz[vertice][j] > 0 && arvores[j] == -1)
+                conta(n, matriz, j, arvores, cont);
 }
